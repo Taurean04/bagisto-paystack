66bbec3e84fdaeee3da661e84e0b72bb41d1f704
Taurean04 <ayobamitomi@gmail.com>
2020-12-10 09:30:48 +0100

    bagisto paystack payment method

---
+++ src/Config/paymentmethods.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paystack'  => [
+        'code'        => 'paystack',
+        'title'       => 'Paystack',
+        'description' => 'Paystack',
+        'class'       => 'webkul\Paystack\Payment\Paystack',
+        'active'      => true,
+        'sort'        => 1,
+    ],
+];
